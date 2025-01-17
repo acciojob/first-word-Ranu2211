3bc9195467c26384e72332d146a452564bf9a03c
Ranu <88536226+Ranu2211@users.noreply.github.com>
2025-01-17 18:52:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function firstWord(s) {
   // your code here
-}
+	let temp = s.trim();
+   if(!temp || !temp.includes(' '))
+	   return temp;
 
+let ind = temp.indexOf(' ');
+return temp.slice(0,ind);
+}
 // Do not change the code below
 
 const s = prompt("Enter String:");
